95d6a7fb66775e7a029ed00c69add45c7888ad5a
guoxc <qazwsxedc121@gmail.com>
2015-10-04 23:58:35 +0800

    init

---
+++ src/qazwsxedc121/calculator0/MainActivity.java
@@ -0,0 +1,15 @@
+package qazwsxedc121.calculator0;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
